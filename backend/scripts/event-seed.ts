a7b5efe8ca647366fb08d679273db2e5a84d7edf
Maxik <mpoplavskiy38380@gmail.com>
2024-05-11 10:48:55 +0300

    feat: add seed script

---
+++ backend/scripts/event-seed.ts
@@ -0,0 +1,25 @@
+import { Pool } from 'pg';
+import 'dotenv/config'
+
+import * as fs from 'fs';
+
+const pool = new Pool({
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+const sql = fs.readFileSync('./scripts/seed.sql').toString();
+console.log(sql);
+
+pool.query(sql)
+  .then(() => {
+    console.log('SQL script executed successfully');
+    pool.end(); // Close the pool
+  })
+  .catch((err) => {
+    console.error('Error executing SQL script:', err);
+    pool.end(); // Close the pool
+  });
