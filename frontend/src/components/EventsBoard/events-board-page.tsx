6a2acad2ca880069f22b44384feae73e28617162
Maxik <mpoplavskiy38380@gmail.com>
2024-05-11 12:39:02 +0300

    fix: events page margings and paddings

--- frontend/src/components/EventsBoard/events-board-page.tsx
+++ frontend/src/components/EventsBoard/events-board-page.tsx
@@ -2,12 +2,12 @@ import EventBoardCard from "./components/event-board-card/event-board-card";
 import styles from './styles.module.scss'
 
 function EventsBoardPage() {
-    return <>
+    return <div className={styles.events_board__page_container}>
         <h1>
             Events
         </h1>
         
-        <main className={styles.events_board_container}>
+        <main className={styles.events_board__container}>
             <EventBoardCard title="123" description="123" />
             <EventBoardCard title="123" description="123" />
             <EventBoardCard title="123" description="123" />
@@ -15,7 +15,7 @@ function EventsBoardPage() {
             <EventBoardCard title="123" description="123" />
             <EventBoardCard title="123" description="123" />
         </main>
-    </>;
+    </div>;
 }
 
 export default EventsBoardPage;
