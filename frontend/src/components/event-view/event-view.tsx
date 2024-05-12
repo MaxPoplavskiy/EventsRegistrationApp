fc4a2b323df3a876ed6ab2d851af0d15e8358e78
Maxik <mpoplavskiy38380@gmail.com>
2024-05-12 14:51:45 +0300

    feat: add search by fullname and email

--- frontend/src/components/event-view/event-view.tsx
+++ frontend/src/components/event-view/event-view.tsx
@@ -1,15 +1,21 @@
 import { useParams } from 'react-router-dom';
 import styles from './styles.module.scss'
 import Event from '../../common/types/event.type';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import eventsRegistrationAppApi from '../../apis/events-registration-app/events-registration-app.api';
 import EventViewCard from './components/event-view-card/event-view-card';
+import Participant from '../../common/types/participant.type';
+
 
 function EventsViewPage() {
     const { id: eventId } = useParams();
 
     const [event, setEvent] = useState<Event | null>(null);
 
+    const [{ email, fullName }, setSearchParams] = useState<Pick<Participant, 'fullName' | 'email'>>({
+        fullName: '',
+        email: ''
+    });
 
     useEffect(() => {
         (async function() {
@@ -19,18 +25,29 @@ function EventsViewPage() {
         })();
     }, [eventId]);
 
-    useEffect(() => {
-        console.log(event);
+    const filterParticipant = useCallback((participant: Participant) => {
+        console.log(fullName);
         
-    }, [event])
+        const fullNameMatch = participant.fullName.toLowerCase().includes(fullName.toLowerCase());
+        const emailMatch = participant.email.toLowerCase().includes(email.toLowerCase());
+        return fullNameMatch && emailMatch;
+    }, [email, fullName]);
+
+    const handleInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setSearchParams((s) => ({ ...s, [name]: value }));
+    }, [setSearchParams]);
 
     return <div className={styles.events_view__page_container}>
         <h1>
             {event?.title} participants
         </h1>
+
+        <input onChange={handleInputChange} name='fullName' className={styles.events_view__input} placeholder='full name'></input>
+        <input onChange={handleInputChange} name='email' className={styles.events_view__input} placeholder='email'></input>
         
         <main className={styles.events_view__container}>
-            {event?.participants?.map((participant) => <EventViewCard email={participant.email} fullName={participant.fullName} />)}
+            {event?.participants?.filter(filterParticipant).map((participant) => <EventViewCard email={participant.email} fullName={participant.fullName} />)}
         </main>     
     </div>;
 }
